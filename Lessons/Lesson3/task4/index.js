a3e7fa1267b3d8ac57fe24fc7403439ce1b2871f
MaksShadows <Lykanenko1995@gmail.com>
2019-11-04 17:56:06 +0200

    task4

---
+++ Lessons/Lesson3/task4/index.js
@@ -0,0 +1,17 @@
+/* оператор || (OR) */
+false || true;
+false || true || false;
+false || false || false;
+
+console.log("false || true: ", false || true);
+console.log("false || true || false: ", false || true || false);
+console.log("false || false || false: ", false || false || false);
+
+/* оператор && (AND) */
+false && true;
+false && true && false;
+true && true && true;
+
+console.log("false && true: ", false && true);
+console.log("false && true && false: ", false && true && false);
+console.log("true && true && true: ", true && true && true);
