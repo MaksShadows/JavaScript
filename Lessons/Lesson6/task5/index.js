ab44d08cbecf8d75da318e34746ef991b0f877dd
MaksShadows <Lykanenko1995@gmail.com>
2019-11-06 19:22:26 +0200

    task5

---
+++ Lessons/Lesson6/task5/index.js
@@ -0,0 +1,10 @@
+function  squareArray(arr){
+  let Array = [];
+  if(!Array.isArray(arr)){
+    return null;
+  }
+  for (let i= 0; i<length.arr; i++){
+    Array.push(arr[i]* arr[i])
+  }
+  return Array;
+}
