77e6d32f6bb2197f3f92b5bce9b7f07514a90f98
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 15:26:03 +0200

    task4

---
+++ Lessons/Lesson10/task4/index.js
@@ -0,0 +1,11 @@
+const getMaxAbsoluteNumber = arr => {
+  if (!Array.isArray(arr) || arr.length == 0) {
+      return null;
+  }
+
+  const absoluteValues = arr.map(num => Math.abs(num));
+
+  const max = Math.max(...absoluteValues);
+
+  return max;
+}
