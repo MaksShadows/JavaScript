eeb8ed00b304890e9ebef45cc45b110d47f91b69
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 14:56:02 +0200

    task2

---
+++ Lessons/Lesson10/task2/index.js
@@ -0,0 +1,7 @@
+const getParsedIntegers = array => array.map(el => Number.parseInt(el));
+
+const getParsedIntegersV2 = array => array.map(el => parseInt(el));
+
+const getParsedFloats = array => array.map(el => Number.parseFloat(el));
+
+const getParsedFloatsV2 = array => array.map(el => parseFloat(el));
