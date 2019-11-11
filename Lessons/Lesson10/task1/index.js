fcf5425369c5f8a3119a57bd8928f372bdcc1985
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 14:43:19 +0200

    task1

--- Lessons/Lesson10/task1/index.js
+++ Lessons/Lesson10/task1/index.js
@@ -6,8 +6,4 @@ const getNaN = array => array.filter(arr => Number.isNaN(arr));
 
 const getNaNV2 = array => array.filter(arr => isNaN(arr));
 
- console.log(getFiniteNumbers(arr));
- console.log(getFiniteNumbersV2(arr));
- console.log(getNaN(arr));
- console.log(getNaNV2(arr));
- console.log(getIntegers(arr));
+ 
