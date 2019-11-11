fd3234dab0cc1f74dcd349394d5a66963f72384c
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 16:05:20 +0200

    task7

---
+++ Lessons/Lesson10/task7/index.js
@@ -0,0 +1,10 @@
+const superRound = (num, drop) => {
+  const sum = Math.pow(10, drop);
+  return [
+      Math.floor(num * sum) / sum,
+      Math.round(num * sum) / sum,
+      Math.ceil(num * sum) / sum,
+      Math.trunc(num * sum) / sum,
+      +num.toFixed(drop),
+  ];
+}
