de9d680c6d5454b58c7f772c364fa1b4bf25f7e5
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 17:53:35 +0200

    task1

---
+++ Lessons/Lesson11/task1/index.js
@@ -0,0 +1,16 @@
+const splitText = (text, len) => {
+
+  if (typeof text !== 'string') return null;
+
+  const arr = [];
+  let startPosition = 0;
+
+  while (true) {
+      let chunk = text.substr(startPosition, len);
+      if (chunk.length === 0) break;
+      arr.push(chunk[0].toUpperCase() + chunk.slice(1));
+      startPosition += len;
+  }
+
+  return arr.join('\n');
+}
