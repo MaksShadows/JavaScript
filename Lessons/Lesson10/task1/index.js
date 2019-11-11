3c7953f198ea367926b62eab19dc2885914ac32f
MaksShadows <Lykanenko1995@gmail.com>
2019-11-11 14:48:55 +0200

    task1

--- Lessons/Lesson10/task1/index.js
+++ Lessons/Lesson10/task1/index.js
@@ -1,9 +1,7 @@
-const getFiniteNumbers = array => array.filter(arg => Number.isFinite(arg));
-
-const getFiniteNumbersV2 = array => array.filter(arg => isFinite(arg));
-
-const getNaN = array => array.filter(arg => Number.isNaN(arg));
-
-const getNaNV2 = array => array.filter(arg => isNaN(arg));
+const getFiniteNumbers = array => array.filter(a => Number.isFinite(a));
+const getFiniteNumbersV2 = array => array.filter(a => isFinite(a));
+const getNaN = array => array.filter(a => Number.isNaN(a));
+const getNaNV2 = array => array.filter(a => isNaN(a));
+const getIntegers = array => array.filter(a => Number.isInteger(a));
 
  
