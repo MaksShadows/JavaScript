2b6c8b1443db6e0cc17aa9bcedb65539aab39825
MaksShadows <Lykanenko1995@gmail.com>
2019-11-12 13:26:31 +0200

    task3

--- Lessons/Lesson12/task3/index.js
+++ Lessons/Lesson12/task3/index.js
@@ -1,3 +1,3 @@
 const cleanTransactionsList = arr => {
-  return arr.filter(i => typeof + i === 'number').map(i => '$' + (+i).toFixed(2));
+  return arr.filter(i => typeof + i === 'number' && !isNaN(i)).map(i => '$' + (+i).toFixed(2));
 }
