c730cd42312977c7e8bf1912b618c30c362e03e2
MaksShadows <Lykanenko1995@gmail.com>
2019-11-12 15:33:13 +0200

    task4

---
+++ Lessons/Lesson13/task4/calculator.test.js
@@ -0,0 +1,31 @@
+import { calc } from './calculator';
+
+it('should get +', () => {
+    const result = calc('45 + 5');
+
+    expect(result).toEqual('45 + 5 = 50');
+});
+
+it('should get -', () => {
+    const result = calc('45 - 5');
+
+    expect(result).toEqual('45 - 5 = 40');
+});
+
+it('should get *', () => {
+    const result = calc('45 * 5');
+
+    expect(result).toEqual('45 * 5 = 225');
+});
+
+it('should get /', () => {
+    const result = calc('45 / 5');
+
+    expect(result).toEqual('45 / 5 = 9');
+});
+
+it('should get string', () => {
+    const result = calc(5);
+
+    expect(result).toEqual(null);
+});
