ea8f898690fffe37d5b166f4f59e1dd98ad78e4e
MaksShadows <Lykanenko1995@gmail.com>
2019-11-12 19:15:03 +0200

    task4

--- Lessons/Lesson13/task4/calculator.test.js
+++ Lessons/Lesson13/task4/calculator.test.js
@@ -1,26 +1,31 @@
 import { calc } from './calculator';
 
-it('should get +', () => {
+it('should get  sum', () => {
     const result = calc('45 + 5');
+
     expect(result).toEqual('45 + 5 = 50');
 });
 
-it('should get -', () => {
+it('should get  difference', () => {
     const result = calc('45 - 5');
+
     expect(result).toEqual('45 - 5 = 40');
 });
 
-it('should get *', () => {
+it('should get division', () => {
     const result = calc('45 * 5');
+    
     expect(result).toEqual('45 * 5 = 225');
 });
 
-it('should get /', () => {
+it('should get multiplication', () => {
     const result = calc('45 / 5');
+
     expect(result).toEqual('45 / 5 = 9');
 });
 
 it('should get string', () => {
     const result = calc(5);
+    
     expect(result).toEqual(null);
 });
