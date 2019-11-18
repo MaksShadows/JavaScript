6e18f14dfb4a3640aa24e102fbbb0284733fee13
MaksShadows <Lykanenko1995@gmail.com>
2019-11-18 17:15:20 +0200

    task3

---
+++ Lessons/Lesson15/task3/index.js
@@ -0,0 +1,41 @@
+export const createLogger = () => {
+
+  const result = [];
+
+  return {
+      warn: text => {
+          result.push({
+              message: text,
+              dateTime: new Date(),
+              type: 'warn',
+
+          })
+      },
+
+      error: text => {
+          result.push({
+              message: text,
+              dateTime: new Date(),
+              type: 'error',
+
+          })
+      },
+
+      log: text => {
+          result.push({
+              message: text,
+              dateTime: new Date(),
+              type: 'log',
+
+          })
+      },
+
+      getRecords: type => {
+          if (type == undefined) {
+              return result.sort((a, b) => b.dateTime - a.dateTime);
+          } else {
+              return result.filter(i => i.type == type)
+          }
+      },
+  }
+};
