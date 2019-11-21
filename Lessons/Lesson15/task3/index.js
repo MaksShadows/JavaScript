1e8b9138a764c9562ed24e93f52634e141b5a896
MaksShadows <Lykanenko1995@gmail.com>
2019-11-21 16:47:13 +0200

    3

--- Lessons/Lesson15/task3/index.js
+++ Lessons/Lesson15/task3/index.js
@@ -31,7 +31,7 @@ export const createLogger = () => {
         },
 
         getRecords: type => {
-            if (type != undefined ) {
+            if (type !== undefined ) {
                 return result.sort((a, b) => b.dateTime - a.dateTime);
             } else {
                 return result.filter(i => i.type == type)
