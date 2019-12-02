dd161d9f823059523687b8facfd92c3b02bc39b9
MaksShadows <Lykanenko1995@gmail.com>
2019-12-02 15:12:35 +0200

    task5

--- Lessons/Lesson17/task5/index.js
+++ Lessons/Lesson17/task5/index.js
@@ -1,28 +1,26 @@
 const timer = {
-  secondsPassed: 0,
-  minsPassed: 0,
-  clearIntervalID: 0,
-
-
-  getTime() {
-      return `${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
-  },
-  startTimer() {
-      this.setInterval(() => {
-        this.secondsPassed += 5
-          if (this.secondsPassed == 60) {
-              this.minsPassed += 1;
-              this.secondsPassed = 0;
-          }
-      }, 5000);
-  },
-  stopTimer() {
-    clearInterval(this.clearIntervalID);
-  },
-  resetTimer() {
-      this.minsPassed = 0;
-      this.secondsPassed = 0;
-  },
+    secondsPassed: 0,
+    minsPassed: 0,
+    clearIntervalID: 0,
+    getTime() {
+        return `${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
+    },
+    startTimer() {
+        this.clearIntervalID = setInterval(() => {
+            this.secondsPassed += 5
+            if (this.secondsPassed === 60) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
+            }
+        }, 5000);
+    },
+    stopTimer() {
+        clearInterval(this.clearIntervalID);
+    },
+    resetTimer() {
+        this.minsPassed = 0;
+        this.secondsPassed = 0;
+    },
 }
 
 export { timer };
