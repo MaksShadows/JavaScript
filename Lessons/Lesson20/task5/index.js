5c2d633a12e509e49b82272623a77a14b8997eb8
MaksShadows <Lykanenko1995@gmail.com>
2019-12-03 13:41:20 +0200

    5

--- Lessons/Lesson20/task5/index.js
+++ Lessons/Lesson20/task5/index.js
@@ -1,28 +1,28 @@
-export class User {
-
-  constructor(id, name, sessionId) {
-      this._id = id;
-      this._name = name;
-      this._sessionId = sessionId;
-  }
+class User {
+    constructor(id, name, sessionId) {
+        this._id = `${id}`;
+        this._name = `${name}`;
+        this._sessionId = `${sessionId}`;
+    };
 };
 
-export class UserRepository {
+class UserRepository {
+    constructor(arr) {
+        this._users = Object.freeze(arr);
+    };
 
-  constructor(users) {
-      this._users = Object.freeze(users);
-  }
+    getUserNames() {
+        return this._users.map(i => i._name);
+    };
 
-  getUserNames() {
-      return this._users.map(({ _name }) => _name);
-  }
+    getUserIds() {
+        return this._users.map(i => i._id);
+    };
 
-  getUserIds() {
-      return this._users.map(({ _id }) => _id);
-  }
-  
-  getUserNameById(userId) {
-      const user = this._users.find(({ _id }) => _id === userId);
-      return user ? user._name : null;
-  }
+    getUserNameById(userId) {
+        let findById = this._users.find(i => i._id === userId);
+        return findById === undefined ? [] : findById._name;
+    };
 };
+
+export { User, UserRepository };
