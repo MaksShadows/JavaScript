dac0f89fbc5c02df17a810fcb68fb588b56a86ff
MaksShadows <Lykanenko1995@gmail.com>
2019-12-05 15:16:48 +0200

    task5

--- Lessons/Lesson20/task5/index.js
+++ Lessons/Lesson20/task5/index.js
@@ -1,26 +1,30 @@
-class User {
-    constructor(id, name, sessionld) {
-        this._id = id;
-        this._name = name;
-        this._sessionld = sessionld;
-    }
-}
+  class User {
+    constructor(id, name, sessionId) {
+        this._id = `${id}`;
+        this._name = `${name}`;
+        this._sessionId = `${sessionId}`;
+    };
+};
+
 
 class UserRepository {
-    constructor(users) {
-        this._users = Object.freeze(users);;
-    }
+    constructor(arr) {
+        this._users = Object.freeze(arr);
+    };
 
     getUserNames() {
-        return this._users.map( ({ _name}) => _name)
+        return this._users.map(i => i._name);
     };
+
     getUserIds() {
-        return this._users.map( ({ _id}) => _id)
+        return this._users.map(i => i._id);
     };
+
     getUserNameById(userId) {
-        let userName = this._users.find(i => i._id === userId)
-        return userName === undefined ? [] : userName._name ;
+        let findById = this._users.find(i => i._id === userId);
+        return findById === undefined ? [] : findById._name;
     };
-}
+};
+
 
 export { User, UserRepository }
