49a36bd5da106adc934f2ae422584cc68f943c82
MaksShadows <Lykanenko1995@gmail.com>
2019-12-05 19:49:38 +0200

    5

--- Lessons/Lesson20/task5/index.js
+++ Lessons/Lesson20/task5/index.js
@@ -24,7 +24,7 @@ class UserRepository {
     };
 
     get users(){
-        return `${id} ${name} ${sessionId}`;
+        return `${this.id} ${this.name} ${this.sessionId}`;
     }
 
     getUserNames() {
