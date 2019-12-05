6d3248dc7baa698edc055d516cd9f0409ab03e50
MaksShadows <Lykanenko1995@gmail.com>
2019-12-05 20:23:10 +0200

    5

--- Lessons/Lesson20/task5/index.js
+++ Lessons/Lesson20/task5/index.js
@@ -23,8 +23,8 @@ class UserRepository {
         this._users = Object.freeze(users);
     };
 
-    get  UserRepository () {
-        return users  ;
+    get users () {
+        return this._users ;
     }
 
     getUserNames() {
