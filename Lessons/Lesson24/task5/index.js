7f60755b2eb2913eabe2a716602f2119f0efc71d
MaksShadows <Lykanenko1995@gmail.com>
2019-12-24 16:20:52 +0200

    5

--- Lessons/Lesson24/task5/index.js
+++ Lessons/Lesson24/task5/index.js
@@ -1,10 +1,10 @@
 const task = [
   { text: 'Buy milk', done: false, id:1, date: new Date(2010, 9, 1, 0, 0, 0, 0),
-  dateDone: null},
+  dateDone: undefined },
   { text: 'Pick up Tom from airport', done: false, id:2, data: new Date(2011, 6, 1, 0, 0, 0, 0),
-  dateDone: null },
+  dateDone: undefined },
   { text: 'Visit party', done: false, id:3,  date: new Date(2015, 3, 1, 0, 0, 0, 0),
-  dateDone: null },
+  dateDone: undefined },
   { text: 'Visit doctor', done: true, id:4,  date: new Date(2018, 5, 1, 0, 0, 0, 0),
   dateDone: new Date( 2020, 8, 8 ) },
   { text: 'Buy meat', done: true, id:5,  date: new Date(2019, 8, 1, 0, 0, 0, 0),
@@ -15,15 +15,18 @@ const rederListItem = listItem => {
   const listElement = document.querySelector('.list');
 
   const listItemElements = listItem
-      .sort((a, b) => b.date - a.date)
       .sort((a, b) => a.done - b.done)
+      .sort((a, b) => b.date - a.date)
+      .sort((a, b) => b.dateDone - a.dateDone)
       .map(({ text, done, id }) => {
           const listItemElem = document.createElement('li')
           listItemElem.classList.add('list__item');
           listItemElem.setAttribute('id', `${id}`);
+
           const checkboxElem = document.createElement('input');
           checkboxElem.setAttribute('type', 'checkbox')
           checkboxElem.checked = done;
+
           if (done) listItemElem.classList.add('list__item_done')
           checkboxElem.classList.add('list__item-checkbox')
           listItemElem.append(checkboxElem, text)
@@ -37,14 +40,14 @@ const rederListItem = listItem => {
 
 rederListItem(task);
 
-const btnAddIvent = document.querySelector('.btn-сreate');
-btnAddIvent.addEventListener('click',  creatEvent);
+const btnAddEvent = document.querySelector('.btn-сreate');
+btnAddEvent.addEventListener('click',  creatEvent);
 const listElement  = document.querySelector('.list')
 listElement.addEventListener('click', check);
 const taskInput = document.querySelector('.task-input');
 
 
-const creatEvent = function() {
+const creatEvent = () =>{
   const  taskInput = document.querySelector('.task-input')
 
   if (taskInput.value == '') return;
@@ -54,16 +57,19 @@ const creatEvent = function() {
     done: false,
     text: taskInput.value,
     data: new Date(),
-    dateDone: null,
+    dateDone: undefined,
   });
   taskInput.value = '';
   rederListItem(task);
 };
   
 
-const check = function(event) {
+const check = (event) => {
+
   const task = tasks.find(elem => elem.id === +event.target.parentElement.id);
   task.done = event.target.checked;
+  task.dateDone = task.done ? new Date() : undefined;
+
   rederListItem(task);
 }
 
