dc7b1895f8fa14cb979900a36487d0d6198c91cc
MaksShadows <Lykanenko1995@gmail.com>
2020-01-21 20:23:07 +0200

    task3

---
+++ Lessons/Lesson27/task3/scripts/todoList.js
@@ -0,0 +1,13 @@
+import { onCreateTask } from './createTask.js';
+import { onToggleTask } from './updateTask.js';
+
+
+function initTodoListHandlers() {
+  const createBtnElem = document.querySelector('.btn-сreate');
+  createBtnElem.addEventListener('click', onCreateTask);
+
+  const todoListElem = document.querySelector('.list');
+  todoListElem.addEventListener('click', onToggleTask);
+};
+
+export { initTodoListHandlers };
