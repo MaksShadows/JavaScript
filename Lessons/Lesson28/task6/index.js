3aac4a3e45e61b7f432a7630637fb5027c9db38c
MaksShadows <Lykanenko1995@gmail.com>
2020-01-22 16:15:39 +0200

    54454

--- Lessons/Lesson28/task6/index.js
+++ Lessons/Lesson28/task6/index.js
@@ -1,11 +1,12 @@
 export const maxFibonacci = (num) => {
+  if (num < 1) return 0;
   let a = 1;
   let b = 1;
   for (let i = 0; i <= num; i++) {
       let c = a + b;
       a = second;
       b = c;
-      if (c >num) return a
+      if (c > num) return a
       if (c == num) return b
   }
 }
