e1153928a00bd1d83896227c82a17c8b6dc94ed0
MaksShadows <Lykanenko1995@gmail.com>
2020-01-23 14:21:35 +0200

    task2

---
+++ Lessons/Lesson29/task2/index.js
@@ -0,0 +1,14 @@
+// pinger(num, period)
+
+export const pinger = (num,period) => {
+    let i = num;
+    console.log('Ping');	
+    const interval = setInterval(() => {
+        if(--i > 0){
+            console.log('Ping');	
+        }else{
+            clearInterval(interval);
+        }
+    }, period);
+};
+//pinger(5,1000);
