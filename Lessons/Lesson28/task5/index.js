92c09b574e6f66e451e3e8719340eaed02e90349
MaksShadows <Lykanenko1995@gmail.com>
2020-01-23 12:55:59 +0200

    task5

--- Lessons/Lesson28/task5/index.js
+++ Lessons/Lesson28/task5/index.js
@@ -14,14 +14,13 @@ export const shmoment = date => {
           return this;
       },
       subtract(type, value) {
-
-          if (type === 'years') result = new Date(result.getFullYear() - value);
-          if (type === 'months') result = new Date(result.getMonth() - value);
-          if (type === 'days') result = new Date(result.getDate() - value);
-          if (type === 'hours') result = new Date(result.getHours() - value);
-          if (type === 'minutes') result = new Date(result.getMinutes() - value);
-          if (type === 'seconds') result = new Date(result.getSeconds() - value);
-          if (type === 'milliseconds') result = new Date(result.getMilliseconds() - value);
+        if (type === 'years') result = new Date(result.setFullYear(result.getFullYear() - value));
+        if (type === 'months') result = new Date(result.setMonth(result.getMonth() - value));
+        if (type === 'days') result = new Date(result.setDate(result.getDate() - value));
+        if (type === 'hours') result = new Date(result.setHours(result.getHours() - value));
+        if (type === 'minutes') result = new Date(result.setMinutes(result.getMinutes() - value));
+        if (type === 'seconds') result = new Date(result.setSeconds(result.getSeconds() - value));
+        if (type === 'milliseconds') result = new Date(result.setMilliseconds(result.getMilliseconds() - value));
           return this;
       },
       result() {
