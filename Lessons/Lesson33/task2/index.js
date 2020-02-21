47884c43ee15de73925f2fcc17e1941ed7001426
MaksShadows <Lykanenko1995@gmail.com>
2020-02-21 13:12:30 +0200

    2

--- Lessons/Lesson33/task2/index.js
+++ Lessons/Lesson33/task2/index.js
@@ -1,45 +1,43 @@
-const userAvatarElem = document.querySelector('.user__avatar');
+const userAvatarElem = document.querySelector('.user__avatar'); 
 const userNameElem = document.querySelector('.user__name');
-const showUserBtnElem = document.querySelector('.name-form__btn');
-
-const userIdInputElem = document.querySelector('.id-form__input');
-const userRepoInputElem = document.querySelector('.repo-form__input');
-const userDaysInputElem = document.querySelector('.days-form__input');
-
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
 
+
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userIdInputEelem = document.querySelector('.id-form__input');
+const userRepoInputEelem = document.querySelector('.repo-form__input');
+const userDaysInputEelem = document.querySelector('.days-form__input');
+
+
 const fetchUserData = userName => {
-    return fetch(`https://api.github.com/users/${userName}`)
+	return fetch(`https://api.github.com/users/${userName}`)
         .then(response => response.json());
 };
-
 const renderUserData = userData => {
-
-    const { avatar_url, name } = userData;
-    userAvatarElem.src = avatar_url;
-    userNameElem.textContent = name;
-
+	const { avatar_url, name } = userData;
+	userAvatarElem.src = avatar_url;
+	userNameElem.textContent = name;
 };
-
 const onSearchUser = () => {
-    const userName = userIdInputElem.value;
-    fetchUserData(userName)
+	const userName = userIdInputEelem.value;
+	fetchUserData(userName)
         .then(userData => renderUserData(userData));
 };
-
 showUserBtnElem.addEventListener('click', onSearchUser);
 
 
-const getUserObject = () => { 
-    const userId = userIdInputElem.value;
-    const repoId = userRepoInputElem.value;
-    const days = userDaysInputElem.value;
+
+
+const forGetUserObject = () => {
+    const days = userDaysInputEelem.value;
+    const userId = userIdInputEelem.value;
+    const repoId = userRepoInputEelem.value;
     getMostActiveDevs({ userId, repoId, days });
 };
-
-showUserBtnElem.addEventListener('click', getUserObject);
+showUserBtnElem.addEventListener('click', forGetUserObject);
 
 
 export const getMostActiveDevs = ({ userId, repoId, days }) => {
