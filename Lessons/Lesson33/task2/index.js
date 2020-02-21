dae1ba3e6ca30022e65326deeacb89492e599a14
MaksShadows <Lykanenko1995@gmail.com>
2020-02-21 13:22:37 +0200

    2

--- Lessons/Lesson33/task2/index.js
+++ Lessons/Lesson33/task2/index.js
@@ -1,15 +1,15 @@
 const userAvatarElem = document.querySelector('.user__avatar'); 
 const userNameElem = document.querySelector('.user__name');
+const showUserBtnElem = document.querySelector('.name-form__btn');
+
+const userIdInputEelem = document.querySelector('.id-form__input');
+const userRepoInputEelem = document.querySelector('.repo-form__input');
+const userDaysInputEelem = document.querySelector('.days-form__input');
+
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
 
-
-
-const showUserBtnElem = document.querySelector('.name-form__btn');
-const userIdInputEelem = document.querySelector('.id-form__input');
-const userRepoInputEelem = document.querySelector('.repo-form__input');
-const userDaysInputEelem = document.querySelector('.days-form__input');
 
 
 const fetchUserData = userName => {
@@ -31,49 +31,38 @@ showUserBtnElem.addEventListener('click', onSearchUser);
 
 
 
-const forGetUserObject = () => {
+const getObject = () => {
     const days = userDaysInputEelem.value;
     const userId = userIdInputEelem.value;
     const repoId = userRepoInputEelem.value;
     getMostActiveDevs({ userId, repoId, days });
 };
-showUserBtnElem.addEventListener('click', forGetUserObject);
-
+showUserBtnElem.addEventListener('click', getObject);
 
 export const getMostActiveDevs = ({ userId, repoId, days }) => {
     const object = { userId, repoId, days };
     let maxCount = 0;
     const startDate = new Date(new Date().setDate(new Date().getDate() - object.days));
-    return fetch(`https://api.github.com/repos/${object.userId}/${object.repoId}/commits?per_page=100`)
+    fetch(`https://api.github.com/repos/${object.userId}/${object.repoId}/commits?per_page=100`)
         .then(response => response.json())
-        .then(fullArray => {
-           const filteredDateArray = fullArray
-                .map(elem => {
+        .then(array => {
+            const resArr = array.map(({ commit: { author: { name, email, date } } }) => ({ name, email, date }))
+                .filter(elem => new Date(elem.date) > startDate)
+                .reduce((acc, { email, name }) => {
+                    const oldCount = acc[email] ? acc[email].count : 0;
                     return {
-                        name: elem.commit.author.name,
-                        email: elem.commit.author.email,
-                        date: new Date(elem.commit.author.date),
-                    }
-                })
-                .filter(elem => elem.date > startDate)
-                .sort((a,b) => a.name.localeCompare(b.name))
-                .reduce((acc, elem, index, array) => {
-                    let count = 0;
-                    array.forEach(element => {
-                        if(elem.name === element.name) count++;
-                    })
-                    array.splice(0,count-1);
-                    acc.push({
-                        count: count,
-                        name: elem.name,
-                        email: elem.email,
-                    });
-                    return acc;
-                },[]);
-            filteredDateArray.forEach(elem => {
-                if(elem.count > maxCount) maxCount = elem.count;
-            });
-            return filteredDateArray
-                .filter(elem => elem.count === maxCount);
-        });
+                        ...acc,
+                        [email]: { name, email, count: oldCount + 1 }
+                    };
+                }, {});
+
+            const arr = Object.values(resArr);
+            arr.forEach(elem => {
+                if (elem.count > maxCount) {
+                    maxCount = elem.count
+                }
+            })
+            return arr.filter(elem => elem.count === maxCount);
+        })
+
 };
