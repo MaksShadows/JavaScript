287cade6296205c72ff8ac8a40409d6c5dd06877
MaksShadows <Lykanenko1995@gmail.com>
2020-02-21 14:09:48 +0200

    task2

--- Lessons/Lesson33/task2/index.js
+++ Lessons/Lesson33/task2/index.js
@@ -1,83 +1,67 @@
-const inputNameEl = document.querySelector('.form__input-user-id');
-const inputRepoEl = document.querySelector('.form__input-repo-id');
-const inputDaysEl = document.querySelector('.form__input-days');
-const usersListEl = document.querySelector('.users-list');
+const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const showUserBtnElem = document.querySelector('.name-form__btn');
 
-const showBtnEl = document.querySelector('.name-form__btn');
-showBtnEl.addEventListener('click', onSearchDev);
+const userIdInputElem = document.querySelector('.id-form__input');
+const userRepoInputElem = document.querySelector('.repo-form__input');
+const userDaysInputElem = document.querySelector('.days-form__input');
 
-const userAvaEl = document.querySelector('.user__avatar'); 
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+userAvatarElem.src = defaultAvatar;
 
-const defaultAva = 'https://avatars3.githubusercontent.com/u10001';
-userAvaEl.src = defaultAva;
-
-//`https://api.github.com/repos/roslavtseva/calendar/commits?per_page=100`
-
-function getMostActiveDevs({ days, userId, repoId }) {
-    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
-        .then(respons => respons.json())
-        .then(commits => getAllDevelopers(commits, days));
-};
-
-function onSearchDev() {
-    const userId = inputNameEl.value;
-    const repoId = inputRepoEl.value;
-    const days = inputDaysEl.value;
-
-    getMostActiveDevs({userId, repoId,  days }) 
-        .then(users => renderUsers(users));
-};
-
-
-function getStartDate(daysAgo) {
-    const now = new Date();
-    return new Date(now.setDate(now.getDate() - daysAgo));
-};
-
-function getAllDevelopers(commits, days) {
-    const dateFrom = getStartDate(days);
-
-    const usersData = commits
-        .filter(commit => new Date(commit.commit.author.date) - dateFrom > 0)
-        .reduce((acc, commit) => {
-            const id = commit.author.id;
-            acc[id] = acc[id] ? acc[id] : {};
-            const name = acc[id].name ? acc[id].name : commit.commit.author.name;
-            const email = acc[id].email ? acc[id].email : commit.commit.author.email;
-            const count = acc[id].count ? ++acc[id].count : 1;
-            const avatar = acc[id].avatar ? acc[id].avatar : commit.author.avatar_url;
-
-            return { ...acc, [id]: { id, name, email, count, avatar} };
-        }, {});
-
-    const arrUsers = Object.values(usersData)
-        .sort((a, b) => b.count - a.count);
-    const TopDevelopersCommits = arrUsers[0].count;
-
-    return arrUsers.filter(({ count }) => TopDevelopersCommits === count);
-};
-
-
-
-function renderUsers(users) {
-    let listItem = '';
-
-    users.map(({ name, avatar }) => {
-        listItem += `<li class="users-list__item">
-                        <img class="user__avatar" src="${avatar}" alt="User Avatar">
-                        <span class="user__name">${name}</span>
-                    </li>`;
-    });
-    usersListEl.innerHTML = listItem;
+const fetchUserData = userName => {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json());
 };
 
 const renderUserData = userData => {
-    const { count, name, email, avatar } = userData;
 
-    userAvaEl.src = avatar;
-    userCommitEl.textContent = `${ "commits: " + count }`;
-    userNameEl.textContent = `${"name: "+ name}|`;
-    userEmailEl.textContent = `${'email: ' + email}|`
+    const { avatar_url, name } = userData;
+    userAvatarElem.src = avatar_url;
+    userNameElem.textContent = name;
+
 };
 
-export { getMostActiveDevs };
+const onSearchUser = () => {
+    const userName = userIdInputElem.value;
+    fetchUserData(userName)
+        .then(userData => renderUserData(userData));
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
+
+
+const getObject = () => {
+    const userId = userIdInputElem.value;
+    const repoId = userRepoInputElem.value;
+    const days = userDaysInputElem.value;
+    getMostActiveDevs({ userId, repoId, days });
+};
+
+showUserBtnElem.addEventListener('click', getObject);
+
+
+export const getMostActiveDevs = ({ days, userId, repoId }) => { 
+    const commits = { days };
+    let counter = 0;
+    const startDate = new Date(new Date().setDate(new Date().getDate() - commits.days));
+    fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+        .then(response => response.json())
+        .then(arr => {
+            let result = arr.map(({ commit: { author: { name, email, date } } }) => ({ name, email, date }))
+                .filter(item => new Date(item.date) > startDate)
+                .reduce((acc, { email, name }) => {
+
+                    const oldCount = acc[email] ? acc[email].count : 0; 
+                    return {...acc,
+                        [email]: { name, email, count: oldCount + 1 }
+                    };
+                }, {})
+            const array = Object.values(result);
+            array.forEach(elem => {
+                if (elem.count > counter) counter = elem.count;
+
+            });
+            return array.filter(elem => elem.count === counter);
+        });
+};
