d3d134047cd00d3e26d0d86ea817e59ed560e8e6
MaksShadows <Lykanenko1995@gmail.com>
2020-02-21 20:54:28 +0200

    task1

--- Lessons/Lesson34/task1/scripts/tasksGateway.js
+++ Lessons/Lesson34/task1/scripts/tasksGateway.js
@@ -1,39 +1,39 @@
 const baseUrl = 'https://crudcrud.com/api/a9ec75833dfe4bf1b5de1e0797328f48/tasks';
 
-function mapTasks(tasks) {
-  return tasks.map(({ _id, ...rest }) => ({ ...rest, id: _id }));
+const mapTasks = tasks =>
+    tasks.map(({ _id, ...rest }) => ({...task, id: _id }));
+
+export const getTasksList = () => {
+    return fetch(baseUrl)
+        .then(response => response.json())
+        .then(tasks => mapTasks(tasks))
 };
 
-function getTasksList() {
-  return fetch(baseUrl)
-      .then(response => response.json())
-      .then(tasks => mapTasks(tasks))
+export const createTask = taskData => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(taskData)
+    })
 };
 
-function creatTask(taskData) {
-  return fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(taskData),
-  })
+export const updateTask = updatedTaskData => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(updatedTaskData)
+    })
 };
 
-function updateTask(taskId, updatedTaskData) {
-  return fetch(`${baseUrl}/${taskId}`, {
-      method: 'PUT',
-      headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(updatedTaskData),
-  })
+export const deleteTask = (taskId) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'DELETE',
+
+    })
 };
 
-function deleteTask(taskId) {
-  return fetch(`${baseUrl}/${taskId}`, {
-      method: 'DELETE',
-  })
-};
 
-export { getTasksList, creatTask, updateTask, deleteTask };
