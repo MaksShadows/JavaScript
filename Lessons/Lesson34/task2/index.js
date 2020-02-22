ef83e898397a34a17790145181fe898121c257f2
MaksShadows <Lykanenko1995@gmail.com>
2020-02-22 13:35:05 +0200

    2

--- Lessons/Lesson34/task2/index.js
+++ Lessons/Lesson34/task2/index.js
@@ -5,9 +5,6 @@ const btnElem = document.querySelector('.submit-button');
 const errorTextElem = document.querySelector('.error-text');
 
 
-const baseUrl = 'https://crudcrud.com/api/a9ec75833dfe4bf1b5de1e0797328f48/users';
-const formElem = document.querySelector('.login-form');
-
 
 const isRequiredPassword = value => value ? undefined : 'Required password';
 
@@ -30,34 +27,34 @@ const validate = (fieldName, value) => {
         .join('');
 };
 
-const formSubmit = event => {
+
+const baseUrl = 'https://crudcrud.com/api/a9ec75833dfe4bf1b5de1e0797328f48/emailObjects';
+const formElem = document.querySelector('.login-form');
+const onFormSubmit = event => {
     event.preventDefault();
-
-
     const formData = [...new FormData(formElem)]
-        .reduce((acc, [key, value]) => ({...acc, [key]: value }), {});
-
-    dataSave(formData)
-        .then(response => response.json())
-        .then(data => {
-            alert(JSON.stringify(data));
-            formElem.reset();
-        })
-        .catch(() => {
-            errorElem.textContent = 'Failed to create user';
-
-        });
-
-    submitBtnElem.setAttribute('enabled', 'disabled');
-
+        .reduce((acc,[field,value]) => ({...acc, [field]:value}),{});
+   
     return fetch(baseUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(formData),
+    }).then(() => {
+        emailInputElem.value = '';
+        nameInputElem.value = '';
+        passwordInputElem.value = ''; 
+        return fetch(baseUrl)
+            .then(response => response.json())
+            .then(arrayOfUserObjects => { 
+           
+                alert(JSON.stringify(arrayOfUserObjects));
+            });
     })
-}
-
-formElem.addEventListener('submit', formSubmit);
-
+    .catch(error => {
+        pErrorText.textContent = 'Failed to create user';
+        return new Error(console.log(`${error}`));
+    });
+}; 
+formElem.addEventListener('submit', onFormSubmit);
