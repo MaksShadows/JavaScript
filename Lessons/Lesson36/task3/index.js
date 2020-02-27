2e400ef45694d4964cbc72076dde5e3ab5e4438f
Maks1501 <49039860+MaksShadows@users.noreply.github.com>
2020-02-27 18:12:06 +0200

    1

--- Lessons/Lesson36/task3/index.js
+++ Lessons/Lesson36/task3/index.js
@@ -1,29 +1,15 @@
-const getUserData = (userId) => {
+const fetchUser = async userId => fetch(`https://api.github.com/users/${userId}`)
+    .then(response => response.json());
 
-    const baseLink = `https://api.github.com/users/${userId}`
-    const response = fetch(baseLink)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } throw new Error('Loading failed');
-        })
-        .then(value => value.blog)
-        .catch(err => err);
-
-    return response
+export const getUsersBlogs = async usersArray => {
+    let usersData = usersArray
+        .map(elem => fetchUser(elem));
+    let userEmail = await Promise.(usersData)
+    return userEmail
+        .map(({ blog }) => blog);
 }
 
-const getUsersBlogs = async (userIdArr) => {
-    try {
-        const arrOfPromises = userIdArr.map(item => getUserData(item));
 
-        const response = await Promise.all(arrOfPromises)
-        console.log(response)
-        return response;
-    }
-    catch (err) {
-        return console.log(err.message)
-    }
-}
 
-getUsersBlogs(['facebook', 'github', 'steve'])
+getUsersBlogs(['google', 'facebook', 'gaearon'])
+    .then(linksList => console.log(linksList));
