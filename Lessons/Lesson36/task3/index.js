0e75e43ce72f41aecf43015533b45aea265611a9
MaksShadows <Lykanenko1995@gmail.com>
2020-02-27 18:14:25 +0200

    3

--- Lessons/Lesson36/task3/index.js
+++ Lessons/Lesson36/task3/index.js
@@ -1,15 +1,33 @@
-const fetchUser = async userId => fetch(`https://api.github.com/users/${userId}`)
-    .then(response => response.json());
+const getUserData = (userId) => {
 
-export const getUsersBlogs = async usersArray => {
-    let usersData = usersArray
-        .map(elem => fetchUser(elem));
-    let userEmail = await Promise.all(usersData)
-    return userEmail
-        .map(({ blog }) => blog);
+    const baseLink = `https://api.github.com/users/${userId}`
+    const response = fetch(baseLink)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } throw new Error('Loading failed');
+        })
+        .then(value => value.blog)
+        .catch(err => err);
+
+    return response
+}
+
+const getUsersBlogs = async (userIdArr) => {
+    try {
+        const arrOfPromises = userIdArr.map(item => getUserData(item));
+
+        const response = await Promise.all(arrOfPromises)
+        console.log(response)
+        return response;
+    }
+    catch (err) {
+        return console.log(err.message)
+    }
 }
 
 
+getUsersBlogs(["facebook", "google"])
+.then(response => console.log(response))
 
-getUsersBlogs(['google', 'facebook', 'gaearon'])
-    .then(linksList => console.log(linksList));
+export { getUsersBlogs };
