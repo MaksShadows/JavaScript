17075f3a03d08bf894584f0246260e9ea2614ad5
Maks1501 <49039860+MaksShadows@users.noreply.github.com>
2020-02-27 18:08:28 +0200

    Update index.js

--- Lessons/Lesson36/task3/index.js
+++ Lessons/Lesson36/task3/index.js
@@ -1,14 +1,32 @@
-const fetchUser = async userId => fetch(`https://api.github.com/users/${userId}`)
-    .then(response => response.json());
+  
+const getUserData = (userId) => {
 
-export const getUsersBlogs = async usersArray => {
-    let usersData = usersArray
-        .map(elem => fetchUser(elem));
-    let userEmail = await Promise.all(usersData)
-    return userEmail
-        .map(({ blog }) => blog);
+    const baseLink = `https://api.github.com/users/${userId}`
+    const response = fetch(baseLink)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } throw new Error('Loading failed');
+        })
+        .then(value => value.blog)
+        .catch(err => err);
+
+    return response
 }
 
+ 
+export  const getUsersBlogs = async (userIdArr) => {
+    try {
+        const arrOfPromises = userIdArr.map(item => getUserData(item));
+
+        const response = await Promise.all(arrOfPromises)
+        console.log(response)
+        return response;
+    }
+    catch (err) {
+        return console.log(err.message)
+    }
+}
 
 
 getUsersBlogs(['google', 'facebook', 'gaearon'])
