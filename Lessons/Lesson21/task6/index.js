908b0699d14ee5207eb42d0917f2d5da01edda9a
MaksShadows <Lykanenko1995@gmail.com>
2020-03-18 19:45:41 +0200

    6

--- Lessons/Lesson21/task6/index.js
+++ Lessons/Lesson21/task6/index.js
@@ -1,8 +1,8 @@
 function setButton (){
   const elem = document.querySelector('.body');
   elem.innerHTML =  `<button>${buttonText}</button>`;
-  const elem2 =  document.querySelector('.body');
-  elem2.textContent = `<button>${buttonText}</button>`;
+  elem.textContent = `<button>${buttonText}</button>`;
+
 };
 
 
