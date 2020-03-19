5850072e84f7d3a3ebaeb92d69a649fea872010b
Maks1501 <49039860+MaksShadows@users.noreply.github.com>
2020-03-19 16:34:51 +0200

    Update index.js

--- Lessons/Lesson22/task11/index.js
+++ Lessons/Lesson22/task11/index.js
@@ -1,5 +1,5 @@
 const page = document.querySelector('.pagination');
-const pageItem = document.querySelectorAll('.pagination__page');
+const pageElem = document.querySelectorAll('.pagination__page');
 
 function handleClick(event) {
   console.log(event.target.dataset.pageNumber);
