194308a8efed2c5f4589904b36b4543f2379163e
MaksShadows <Lykanenko1995@gmail.com>
2020-03-19 16:04:29 +0200

    task7

---
+++ Lessons/Lesson22/task7/index.js
@@ -0,0 +1,6 @@
+const  btn = document.querySelector('.single-use-btn');
+const getClick = () => {
+    console.log('clicked');
+    btn.removeEventListener('click', getClick);
+}
+btn.addEventListener('click', getClick);
