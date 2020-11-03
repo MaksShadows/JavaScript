0fe5cab136d930f36ce60ce736382223d9056b42
MaksShadows <Lykanenko1995@gmail.com>
2020-11-03 16:09:04 +0200

    111

--- Lessons/Lesson29/task1/index.js
+++ Lessons/Lesson29/task1/index.js
@@ -1,10 +1,10 @@
-export const addImage = (imgSrc, callback) => {
-
+ export const addImage = (imgSrc, callback) => {
+     
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
 
-    const containerElem  = document.querySelector('.page');
+    const containerElem = document.querySelector('.page');
     containerElem.append(imgElem);
 
     const onImageLoaded = () => {
@@ -13,22 +13,22 @@ export const addImage = (imgSrc, callback) => {
     };
 
     imgElem.addEventListener('load', onImageLoaded);
-    imgElem.addEventListener('error', () =>  callback('Image load failed'));
+
+    imgElem.addEventListener('error', () => callback('Image load failed'))
 };
 
+// const imgSrc = 'https://server.com/image.png';
 
-//const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
-//callack example
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
     if (error) {
         console.log(error);
         return;
     }
-    const { width, height } = imgElem;
+    const { width, height } = data;
     const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`;
-}
+    sizeElem.textContent = `${width}: x ${height}`;
+};
 
-addImage(imgSrc, onImageLoaded);
+//test call
+//addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
 
