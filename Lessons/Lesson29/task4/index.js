3c2fba9fac7da8b0cd2ec110f2ad730b05a92157
MaksShadows <Lykanenko1995@gmail.com>
2020-11-03 16:53:18 +0200

    task4

--- Lessons/Lesson29/task4/index.js
+++ Lessons/Lesson29/task4/index.js
@@ -1,20 +1,16 @@
 export const requestUserData = (userId, callback) => {
 
-    setTimeout(() => {
 
-        if (userId === 'broken') {
-            callback(null, 'Failed to load user data');
-            return;
-        }
-
-        callback({
-            name: 'John',
-            age: 17,
-            userId,
-            email: `${userId}@example.com`,
-        })
-    }, period);
-
-    const period = Math.floor(Math.random() * 3 + 1) * 1000;
+  const onLoad = () => {
+      if (userId == 'broken') {
+          callback(null, 'Failed to load user data');
+          return;
+      }
+      callback({
+          userId,
+          email: `${userId}@example.com`,
+      });
+  }
+  setTimeout(onLoad, Math.floor(Math.random() * 3 + 1) * 1000);
 
 };
